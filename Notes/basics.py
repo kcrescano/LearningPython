bc8ede40c64569ba25ebd566911d99d2aa56566d
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-26 14:26:44 -0400

    Create basics.py

---
+++ Notes/basics.py
@@ -0,0 +1,33 @@
+# Comment
+
+# Variable and Data type
+integer = 1
+floating = 1.0
+string = "1"
+
+# print() and type()
+print()
+print("")
+print('')
+print(integer)
+print(type(integer))
+# more built-in functions like int(), float() and str()
+
+# input() and type casting
+name = input("Enter your name: ") # with prompt "Enter your name: "
+name = input() # automatic string str()
+integer = int(input())
+floating_point = float(input())
+
+# Basic Math operations
+addition = 1 + 1
+subtraction = 1 - 1
+multiplication = 1 * 1
+division = 1 / 1  # 1.0 
+int_division = 1 // 1 # 1
+modulo = 3 % 2 # 1
+power = 2 ** 2
+
+# compound assignment
+counter = counter + 1 # or counter += 1
+# same with -=, *=, /=, //=, %=, **=
