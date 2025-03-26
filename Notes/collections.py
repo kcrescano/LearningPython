2c6e6e71a63a4867783006581d7277e5bdd9f600
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-26 16:39:01 -0400

    Create collections.py

---
+++ Notes/collections.py
@@ -0,0 +1,10 @@
+# list
+list_ = list("abcde") # is the same as
+list_ = ["a", "b", "c", "d", "e"]
+# split function to make a list
+list_ = "a b c d e".split() # only with spaces as default
+# list comprehension
+list_ = [elem for elem in input()] # list of str
+list_ = [int(elem) for elem in input()] # list of int
+
+# len() can be use to get how many element a list have (or any iterable) 
