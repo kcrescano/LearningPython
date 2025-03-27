beb7b0a2b7889fe1c9a5fa1b4fab1b4f4ca77553
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 08:47:41 -0400

    Create if_else.py

---
+++ Cool Codes/if_else.py
@@ -0,0 +1,8 @@
+# mine
+n = int(input()) * -1
+print(n)
+print("The number is", "positive" if n > 0 else "negative")
+
+# hyperskill user posted solution
+x = int(input())
+print(f'{-x}\nThe number is', 'pnoesgiattiivvee'[x > 0::2])
