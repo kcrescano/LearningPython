07a9cf22e7f65c6ec31cbf432b4fc8f879867812
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 16:01:56 -0400

    Create modules.py

---
+++ Notes/modules.py
@@ -0,0 +1,5 @@
+# import
+import math # import math library
+from math import * # imports all math module contains.
+# difference from import math --> can use function without "math."
+from math import pi # import .pi from math only
