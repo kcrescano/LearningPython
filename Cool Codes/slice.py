0e4629d0519d90f0fb1bea87dd57bad3fde66597
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 08:38:15 -0400

    Create slice.py

---
+++ Cool Codes/slice.py
@@ -0,0 +1,14 @@
+# mine
+
+ticket = [int(x) for x in input()]
+half1 = sum(ticket[:3])
+half2 = sum(ticket[3:])
+
+if half1 == half2:
+    print("Lucky")
+else:
+    print("Ordinary")
+
+# hyperskill user posted solution
+ticket = [int(x) for x in input()]
+print(sum(ticket[:3]) == sum(ticket[-3:]) and "Lucky" or "Ordinary")
