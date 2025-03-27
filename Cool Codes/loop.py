49ade270940490b9bc01ce0ffef387e54a859ead
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 08:32:42 -0400

    Update and rename forLoop.py to loop.py

--- Cool Codes/loop.py
+++ Cool Codes/loop.py
@@ -6,3 +6,9 @@ print(type(2.12))
 # hyperskill user posted solution
 for i in 'int', 394, 2.71:
     print(type(i))
+
+# mine
+print("1 2 3 4 5 6 7 8 9 10")
+
+# hyperskill user posted solution
+print(*range(1, 11))
