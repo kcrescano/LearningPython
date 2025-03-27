9572bf0337f96317c3901701100984ce2527ba39
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 09:05:00 -0400

    Update if_else.py

--- Cool Codes/if_else.py
+++ Cool Codes/if_else.py
@@ -6,3 +6,17 @@ print("The number is", "positive" if n > 0 else "negative")
 # hyperskill user posted solution
 x = int(input())
 print(f'{-x}\nThe number is', 'pnoesgiattiivvee'[x > 0::2])
+#**************************************************************************
+# mine
+number = int(input())
+word = input()
+
+if number != 1:
+    word += 's'
+
+print(number, word)
+# hyperskill user posted solution
+number = int(input())
+word = input()
+
+print(number, word + "s" * (number != 1))
