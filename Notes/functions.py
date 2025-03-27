a9b65b4ff6fc5068844cf4240fb18e2904d9b474
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 15:58:08 -0400

    Create functions.py

---
+++ Notes/functions.py
@@ -0,0 +1,11 @@
+# function
+
+def function_():
+  return "something"
+# returns a string
+# opposite of void functions
+
+def function_():
+  print()
+# call print() and return None object
+# similiar to void functions
