5424523c88a791a0ddb9bfc4a59e54eb9d9387b7
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-03-27 08:25:49 -0400

    Create printDayofWeek.py

---
+++ Cool Codes/printDayofWeek.py
@@ -0,0 +1,18 @@
+# mine
+print("Sunday")
+print()
+print("Monday")
+print()
+print("Tuesday")
+print()
+print("Wednesday")
+print()
+print("Thursday")
+print()
+print("Friday")
+print()
+print("Saturday")
+
+# hyperskill user posted solution
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+print(*days, sep='\n\n')
