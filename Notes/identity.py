705398c79f6c501b82382fc705ed2486aa64ca17
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-03 13:44:37 -0400

    Update identity.py

--- Notes/identity.py
+++ Notes/identity.py
@@ -3,6 +3,7 @@ print(a is b)
 # Python optimizes the use of small integers between -5 and 256.
 a, b = 10000, 10000
 print(a is b)  # True or False depending on your system
+print(a == b)  # True, == compares value
 
 def say_hello(name=None):
     if name is None:
