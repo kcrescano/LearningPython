12824201f1252f5daf71182b20eec8184bcc2abe
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-03 07:45:41 -0400

    Create identity.py

---
+++ Notes/identity.py
@@ -0,0 +1,15 @@
+a, b = 1, 1
+print(a is b)
+# Python optimizes the use of small integers between -5 and 256.
+a, b = 10000, 10000
+print(a is b)  # True or False depending on your system
+
+def say_hello(name=None):
+    if name is None:
+        print('Hello!')
+    else:
+        print(f'Hello, {name}!')
+
+
+say_hello()        # 'Hello!'
+say_hello('Nick')  # 'Hello, Nick!'
