0878e4bd02aa0b0d94c46df39f0451d5b2ca89a3
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-03 13:47:19 -0400

    Create boolean.py

---
+++ Cool Codes/boolean.py
@@ -0,0 +1,7 @@
+# mine
+def check_values(first_value, second_value):
+    return bool(first_value) and bool(second_value)
+  
+# Hyperskill user posted solution
+def check_values(first_value, second_value):
+    return all([first_value, second_value])
