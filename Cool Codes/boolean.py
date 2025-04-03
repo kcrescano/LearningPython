c49697452ae85805db7b49644506968a9bb5f79b
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-03 14:16:54 -0400

    Update boolean.py

--- Cool Codes/boolean.py
+++ Cool Codes/boolean.py
@@ -1,7 +1,27 @@
 # mine
 def check_values(first_value, second_value):
     return bool(first_value) and bool(second_value)
-  
 # Hyperskill user posted solution
 def check_values(first_value, second_value):
     return all([first_value, second_value])
+#************************************************************************** 
+# mine
+def print_book_info(title, author=None, year=None):
+    book = f'"{title}"'
+    if author or year:
+        book += ' was written'
+    if author:
+        book += f' by {author}'
+    if year:
+        book += f' in {year}'
+    print(book)
+# Hyperskill user posted solution
+def print_book_info(title, author=None, year=None):
+    print(f"\"{title}\"", " was written" if author or year else "",
+          f" by {author}" if author else "",
+          f" in {year}" if year else "", sep="")
+# Hyperskill user posted solution
+def print_book_info(title, author=None, year=None):
+    print(f'"{title}"', ' was written' * bool(author or year), 
+          f' by {author}' * bool(author),
+          f' in {year}' * bool(year), sep='')
