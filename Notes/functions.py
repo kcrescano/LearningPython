46bae6234bc5d848d25b5b67ddf0775059abfc78
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-08 07:59:12 -0400

    Update functions.py

--- Notes/functions.py
+++ Notes/functions.py
@@ -1,11 +1,13 @@
 # function
 
-def function_():
-  return "something"
+def function(x, y):  # x and y are parameters
+  return x + y
 # returns a string
 # opposite of void functions
 
-def function_():
+def function():
   print()
 # call print() and return None object
 # similiar to void functions
+
+print(function(x, y))  # invoking function, x and y here are arguments
