24e5dc4e819536720359d9322429e6dd7f2ff988
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-16 08:54:18 -0400

    Update print.py

--- Cool Codes/print.py
+++ Cool Codes/print.py
@@ -12,7 +12,15 @@ print()
 print("Friday")
 print()
 print("Saturday")
-
 # hyperskill user posted solution
 days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 print(*days, sep='\n\n')
+#**************************************************************************
+# mine
+print("""'
+'"'
+'"'"'
+'"'"'"'""")
+# hyperskill user posted solution
+for i in range(4):
+    print("'" + "\"'" * i)
