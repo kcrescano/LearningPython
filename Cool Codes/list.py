b31a39631435a2bbfb45071f44ea766f3cb1aaa8
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-17 11:45:33 -0400

    Update list.py

--- Cool Codes/list.py
+++ Cool Codes/list.py
@@ -1,7 +1,11 @@
 #mine
 dictionary = ["aa", "abab", "aac", "ba", "bac", "baba", "cac", "caac"]
 print("Correct" if input() in dictionary else "Incorrect")
-
 # hyperskill user posted solution
 dictionary = ["aa", "abab", "aac", "ba", "bac", "baba", "cac", "caac"]
 print(["Incorrect", "Correct"][input() in dictionary])
+#**************************************************************************
+#mine
+print([x for x in range(1, 1000) if x % 3 == 0])
+# hyperskill user posted solution
+print(list(range(3, 1000, 3)))
