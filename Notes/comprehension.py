fc7b1d1a80c8f74d2d986f6638aefb1ae0971851
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-17 11:36:24 -0400

    Update comprehension.py

--- Notes/comprehension.py
+++ Notes/comprehension.py
@@ -3,6 +3,10 @@ numbers = [1, 2, 3, 4, 5]
 squared_numbers = [x**2 for x in numbers]  # [1, 4, 9, 16, 25]
 even_numbers = [x for x in numbers if x % 2 == 0]  # [2, 4]
 
+old_list = [8, 13, -7, 4, -9, 2, 10]
+new_list = [num if num >= 0 else 0 for num in old_list]
+print(new_list)  # [8, 13, 0, 4, 0, 2, 10]
+
 # Dictionary Comprehension
 numbers = [1, 2, 3, 4, 5]
 number_map = {x: x**2 for x in numbers}  # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
