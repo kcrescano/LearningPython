bee37f2e8b594fb38573c42ef37d385895b25568
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-22 09:50:42 -0400

    Delete Notes directory

--- Notes/tuple.py
+++
@@ -1,8 +0,0 @@
-# Tuple is immutable, faster and more memory-efficient lists
-# can be use as dictionary key, unlike list
-
-tuple_ = ()  # empty
-tuple_ = tuple()  # empty, this also turn iterables to tuple
-# comma is what make a tuple
-tuple_ = 'a', 
-tuple_ = ('a', 'b')  
