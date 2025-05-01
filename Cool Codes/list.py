fd89fd1110d060c1d2fff2cb2b5308694218d612
Kurt Escano <150044622+kcrescano@users.noreply.github.com>
2025-05-01 08:15:03 -0400

    Update list.py

--- Cool Codes/list.py
+++ Cool Codes/list.py
@@ -9,3 +9,23 @@ print(["Incorrect", "Correct"][input() in dictionary])
 print([x for x in range(1, 1000) if x % 3 == 0])
 # hyperskill user posted solution
 print(list(range(3, 1000, 3)))
+#**************************************************************************
+#mine
+stack = []
+for _ in range(int(input())):
+    action = input().split()
+    if action[0] == "PUSH":
+        stack.append(action[1])
+    else:
+        stack.pop()
+
+print(*stack[::-1], sep='\n')
+# hyperskill user posted solution
+my_stack = []
+for _ in range(int(input())):
+    try:
+        action, value = input().split(' ')
+        my_stack.append(value)
+    except ValueError:
+        my_stack.pop()
+print(*my_stack[::-1], sep='\n')
