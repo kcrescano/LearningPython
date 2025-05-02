60b6e3a06b34df225ae3284896846c5b71c1bc15
Kurt Escano <150044622+kcrescano@users.noreply.github.com>
2025-05-02 14:03:54 -0400

    Update iterator.py

--- Cool Codes/iterator.py
+++ Cool Codes/iterator.py
@@ -4,3 +4,10 @@ for a, b in zip(v1, v2):
 # Hyperskill user posted solution
 for e in zip(v1, v2):
     print(sum(e))
+#**************************************************************************
+# mine
+for i, name in enumerate(student_list, start=1):
+    print(i, name)
+# Hyperskill user posted solution
+for student in enumerate(student_list, start=1):
+    print(*student)
