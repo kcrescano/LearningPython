b8600dfbed1f155a28829008a3d95f9c11bf03eb
Kurt Escano <150044622+kcrescano@users.noreply.github.com>
2025-05-02 13:01:37 -0400

    Update generator.py

--- Cool Codes/generator.py
+++ Cool Codes/generator.py
@@ -5,3 +5,15 @@ def letters(word):
 # Hyperskill user posted solution
 def letters(word):
     yield from word
+#************************************************************************** 
+# mine
+n = int(input())
+def even(num):
+    yield num * 2
+for i in range(n):
+    print(next(even(i)))
+# Hyperskill user posted solution
+n = int(input())
+def even():
+    yield from range(0, 2 * n, 2)
+print(*even(), sep="\n")
