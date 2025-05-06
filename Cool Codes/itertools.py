82535be15a1c1d61f6e9182133c23bfdb412afc4
Kurt Escano <150044622+kcrescano@users.noreply.github.com>
2025-05-06 10:27:16 -0400

    Create itertools.py

---
+++ Cool Codes/itertools.py
@@ -0,0 +1,9 @@
+# mine
+import itertools
+
+dishes = itertools.product(main_courses, desserts, drinks)
+prices = itertools.product(price_main_courses, price_desserts, price_drinks)
+
+for dish, price in zip(dishes, prices):
+    if (total := sum(price)) <= 30:
+        print(*dish, total)
